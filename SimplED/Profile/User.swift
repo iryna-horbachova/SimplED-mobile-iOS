47df8fcb139044611c7c650683622d136e7a0cb3
iryna-horbachova <irachka.gorbacheva@gmail.com>
2020-10-31 12:15:37 +0200

    Add POST methods for user and course creation

--- SimplED/Profile/User.swift
+++ SimplED/Profile/User.swift
@@ -1,9 +1,9 @@
-public struct User: Decodable {
-  public let id: Int
+public struct User: Codable {
+  public let id: Int?
   public let firstName: String
   public let lastName: String
   public let email: String
-  public let bio: String
-  public let image: String
-  public let points: Int
+  public let bio: String?
+  public let image: String?
+  public let points: Int?
 }
