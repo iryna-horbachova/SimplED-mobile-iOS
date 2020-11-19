077807af10ba88d0cecf3527a85c7b484a803f38
iryna-horbachova <irachka.gorbacheva@gmail.com>
2020-11-20 00:40:28 +0200

    Add api logic for adding solutions

--- SimplED/Courses/SolutionViewController.swift
+++ SimplED/Courses/SolutionViewController.swift
@@ -2,7 +2,10 @@ import UIKit
 
 class SolutionViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
+  var solution: Solution?
   let textField = UITextView.makeTextView() 
+  var taskId: Int?
+  var courseId: Int?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -10,7 +13,7 @@ class SolutionViewController: UIViewController, UITextFieldDelegate, UITextViewD
     title = "Solution to the task"
     navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(addSolution))
 
-    textField.text = "Solution"
+    textField.text = solution?.text ?? "Solution"
     textField.delegate = self
     
     view.addSubview(textField)
@@ -24,5 +27,35 @@ class SolutionViewController: UIViewController, UITextFieldDelegate, UITextViewD
   }
 
   @objc func addSolution() {
+    print("former solution")
+    print(solution)
+    let newSolution = Solution(id: nil, owner: APIManager.currentUser!, task: taskId!, text: textField.text!)
+    
+    APIManager.shared.add(solution: newSolution, courseId: courseId!) { [weak self] result in
+      guard let self = self else { return }
+      switch result {
+      case .success(_):
+        DispatchQueue.main.async {
+          self.present(
+            UIAlertController.alertWithOKAction(
+              title: "Success!",
+              message: "The solution was added successfully!"),
+            animated: true,
+            completion: nil
+          )
+        }
+        
+      case .failure(let error):
+        DispatchQueue.main.async {
+          self.present(
+            UIAlertController.alertWithOKAction(
+              title: "Error occured!",
+              message: error.rawValue),
+            animated: true,
+            completion: nil)
+          
+        }
+      }
+    }
   }
 }
