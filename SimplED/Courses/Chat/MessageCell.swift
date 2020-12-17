577d7311f26aad445cdcd2f5c85ee11fd28d0ea3
iryna-horbachova <irachka.gorbacheva@gmail.com>
2020-12-17 20:03:08 +0200

    Add message

--- SimplED/Courses/Chat/MessageCell.swift
+++ SimplED/Courses/Chat/MessageCell.swift
@@ -2,15 +2,28 @@ import UIKit
 
 class MessageCell: UITableViewCell {
   
+  let messageLabel = UILabel.makeSecondaryLabel()
+  let nameLabel = UILabel()
+  
   override func awakeFromNib() {
     super.awakeFromNib()
     // Initialization code
   }
   
-  override func setSelected(_ selected: Bool, animated: Bool) {
-    super.setSelected(selected, animated: animated)
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
     
-    // Configure the view for the selected state
+    nameLabel.textColor = .lightGray
+    nameLabel.font = UIFont(name: "Helvetica", size: 10)
+
+    clipsToBounds = true
   }
   
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+  }
 }
